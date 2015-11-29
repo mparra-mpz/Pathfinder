90023a23c4ec3ba64244e4fa7d002a7ef891f2dd
mparra-mpz <mparra.mpz@gmail.com>
2015-11-28 22:53:00 -0300

    Pathfinder library will include all the necessary modules to build the rover.

---
+++ arduino/Pathfinder/Direction.h
@@ -0,0 +1,15 @@
+/*
+ * Author: Manuel Parra Z.
+ * Date: 27/11/2015
+ * License: MIT License
+ * Description: Header file for the enumeration
+ * struct of the rover direction.
+ * Link: https://github.com/mparra-mpz/Pathfinder
+ */
+
+#ifndef Direction_h
+#define Direction_h
+
+enum Direction {STOP, FOWARD, REVERSE, LEFT, RIGHT};
+
+#endif
